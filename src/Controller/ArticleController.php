5d8c3d598a32301b4d6f55029cdfbc7ed66327bf
Mark Packham <markpackham1@gmail.com>
2018-03-15 16:33:21 +0000

    articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController
+class ArticleController extends Controller
 {
     /**
      * @Route("/")
@@ -14,6 +15,8 @@ class ArticleController
      */
     public function index()
     {
-        return new Response("<h1>Hello world</h1>");
+        $articles = "Hello Mark";
+        // return new Response("<h1>Hello world</h1>");
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 }
