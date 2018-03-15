432c01943acd0b7077b8d8ab47bf848a8f4e3c38
Mark Packham <markpackham1@gmail.com>
2018-03-15 16:00:10 +0000

    Annotations route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function index()
     {
         return new Response("<h1>Hello world</h1>");
