6cbd232951b78238ace21d17581bbda6a14c6482
Mark Packham <markpackham1@gmail.com>
2018-03-16 10:37:41 +0000

    create new article function in controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,55 +3,90 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 
-class ArticleController extends Controller
-{
-    /**
-     * @Route("/", name="article_list")
-     * @Method({"GET"})
-     */
-    public function index()
-    {
-        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
-        // return new Response("<h1>Hello world</h1>");
-        return $this->render('articles/index.html.twig', array('articles' => $articles));
-    }
+class ArticleController extends Controller {
 
-    /**
-     * @Route("/article/{id}", name="article_show")
-     */
-    public function show($id)
-    {
-        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-        return $this->render('articles/show.html.twig', array('article' => $article));
-    }
+  /**
+   * @Route("/", name="article_list")
+   * @Method({"GET"})
+   */
+  public function index() {
+    $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+    // return new Response("<h1>Hello world</h1>");
+    return $this->render('articles/index.html.twig', ['articles' => $articles]);
+  }
 
-//    /**
-//     * @Route("/article/save")
-//     */
-    /*
-    public function save()
-    {
-        $entityManager = $this->getDoctrine()->getManager();
+  /**
+   * @Route("/article/new", name="new_article")
+   * @Method({"GET","POST"})
+   */
+  public function new(Request $request) {
+    $article = new Article();
+    $form = $this->createFormBuilder($article)
+      ->add('title', TextType::class, [
+        'attr' =>
+          ['class' => 'form-control'],
+      ])
+      ->add('body', TextareaType::class, [
+        'required' => FALSE,
+        'attr' =>
+          [
+            'class' => 'form-control',
+          ],
+      ])
+      //Save button
+      ->add('save', SubmitType::class, [
+        'label' => 'Create',
+        'attr' =>
+        //mt-3 means margin top 3
+          [
+            'class' => 'btn btn-primary mt-3',
+          ],
+      ])->getForm();
 
-        $article = new Article();
-        $article->setTitle('Article Two');
-        $article->setBody('This is the body for article two');
+    return $this->render('articles/new.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
 
-        //take the data here, and later we flush it into the database
-        $entityManager->persist($article);
-        $entityManager->flush();
+  //show needs to go below new_article or you end up with nulls like the message below
+  // "Impossible to access an attribute ("title") on a null variable."
+  /**
+   * @Route("/article/{id}", name="article_show")
+   */
+  public function show($id) {
+    $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+    return $this->render('articles/show.html.twig', ['article' => $article]);
+  }
 
-        return new Response('Saved an article with the id of ' .
-            $article->getId());
-        */
+
+  //    /**
+  //     * @Route("/article/save")
+  //     */
+  /*
+  public function save()
+  {
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $article = new Article();
+      $article->setTitle('Article Two');
+      $article->setBody('This is the body for article two');
+
+      //take the data here, and later we flush it into the database
+      $entityManager->persist($article);
+      $entityManager->flush();
+
+      return new Response('Saved an article with the id of ' .
+          $article->getId());
+      */
 
 }
