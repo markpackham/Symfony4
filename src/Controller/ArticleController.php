9bff150b5284a95073bc51b2a29effd74d998c1e
Mark Packham <markpackham1@gmail.com>
2018-03-16 10:10:47 +0000

    add symfony form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class ArticleController extends Controller
 {
